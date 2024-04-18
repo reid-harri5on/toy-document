353f2c5e860b2e080da39764fc1ae956b4a7e0d8
Reid Harrison <reid.al.harrisson@gmail.com>
2024-04-18 09:54:47 +0200

    feat: create project and add home page background

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,6 @@
+import ImgHomeBack from "../../assets/images/homeback.jpg";
+import { Container } from "./styles";
+
+export const Home = () => {
+  return <Container img={ImgHomeBack}></Container>;
+};
---
+++ src/styles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: black;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+`;
